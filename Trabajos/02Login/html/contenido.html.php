2cc43d0996ceb8f8b8fcf316c9fbd1cbd0e52c44
Yader Campbell <yadermejiac123@gmail.com>
2021-08-06 22:07:12 -0600

    Practica: Proyecto de Login aplicando lo aprendido

---
+++ Trabajos/02Login/html/contenido.html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos minima ut numquam corrupti 
+                    aliquam incidunt, facere id nisi perspiciatis possimus in odit blanditiis iusto ea harum 
+                    dolorum doloremque cumque alias.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos minima ut numquam corrupti 
+                    aliquam incidunt, facere id nisi perspiciatis possimus in odit blanditiis iusto ea harum 
+                    dolorum doloremque cumque alias.</p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
