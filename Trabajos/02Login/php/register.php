cbc07261d117c5deb3a552fbb4f6393d2de8cbc6
Yader Campbell <yadermejiac123@gmail.com>
2021-08-11 16:09:35 -0600

    Proyecto: Validando la entrada de datos de registro

--- Trabajos/02Login/php/register.php
+++ Trabajos/02Login/php/register.php
@@ -1,3 +1,36 @@
 <?php
+    session_start();
+    if(isset($_SESSION['usuario'])){
+        header('Location: ../index.php');
+    }
+
+    //Verificamos si los datos son enviados
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        //Recuperamos los datos
+        $usuario = filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);   //strtolower = convierte los datos en minusculas
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+
+        $errores = '';
+
+        if(empty($usuario) or empty($password) or empty($password2)){
+            $errores .= '<li>Por favor rellena los datos correctamentes</li>';
+        }else{
+            try{
+                $conexion = new PDO('mysql:host=127.0.0.1;dbname=usuarios_db', 'root', '');
+            }catch(PDOException $e){
+               echo "Error: ".$e -> getMessage(); 
+            }
+
+            $statement = $conexion -> prepare('SELECT * FROM usuarios WHERE usuario = :usuario LIMIT 1');
+            $statement -> execute(array(':usuario' => $usuario));
+            $resultado = $statement -> fetch();
+
+            //Si el usuario ya existe
+            if($resultado != false){
+                $errores .= '<li>El nombre del usuario ya existe</li>';
+            }
+        }
+    }
     require('../html/register.html.php');
 ?>
