29431f125c5105813a49e05ec7003f4dff7e1c76
Yader Campbell <yadermejiac123@gmail.com>
2021-08-12 11:11:17 -0600

    Proyecto: Validando contraseña y encriptandola en la base de datos

--- Trabajos/02Login/php/register.php
+++ Trabajos/02Login/php/register.php
@@ -30,6 +30,23 @@
             if($resultado != false){
                 $errores .= '<li>El nombre del usuario ya existe</li>';
             }
+
+            //Validar y encriptar Contraseña
+            $password = hash('sha512', $password);
+            $password2 = hash('sha512', $password2);
+
+            //Validamos que las dos contraseña sean iguales
+            if($password != $password2){
+                $errores .= '<li>Las contraseñas no son iguales</li>';
+            }
+        }
+
+        //Subimos los datos en la base de datos si no se encontraron errores
+        if($errores == ''){
+            $statement = $conexion -> prepare('INSERT INTO usuarios (id, usuario, pass) VALUE (NULL, :usuario, :pass)');
+            $statement -> execute(array(':usuario' => $usuario, ':pass' => $password));
+
+            header('Location: ./login.php');
         }
     }
     require('../html/register.html.php');
