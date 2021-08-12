979a7c0f660a38ba8207d66d30b27dd45b7fc02f
Yader Campbell <yadermejiac123@gmail.com>
2021-08-12 11:58:54 -0600

    Proyecto: Cerrando session y protegiendo el contenido

--- Trabajos/02Login/php/contenido.php
+++ Trabajos/02Login/php/contenido.php
@@ -1,3 +1,9 @@
 <?php
-    require('../html/contenido.html.php');
+    session_start();
+
+    if (isset($_SESSION['usuario'])) {
+        require('../html/contenido.html.php');
+    }else{
+        header('Location: ./login.php');
+    }
 ?>
